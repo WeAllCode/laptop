a753cb6d85efe45d322637bd9da179903843488e
Roger Wakeman <rgrwkmn@users.noreply.github.com>
2023-08-18 21:13:05 -0500

    Update background before opening survey
    I like this better.

--- logon.script.sh
+++ logon.script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Update Dock
+# don't set background before updating the dock, it makes the dock update not work sometimes
 /usr/local/bin/dockutil --remove all \
     --add /Applications/Google\ Chrome.app \
     --add /Applications/Firefox.app \
@@ -9,10 +10,10 @@
 
 # ---------------------------------------------
 
-# Open Survey
-open https://wac.fyi/survey
+# Set Background
+automator -i /Users/Shared/weallcode-background.png /Users/Shared/setDesktopWallpaper.workflow
 
 # ---------------------------------------------
 
-# Set Background
-automator -i /Users/Shared/weallcode-background.png /Users/Shared/setDesktopWallpaper.workflow
+# Open Survey
+open https://wac.fyi/survey
