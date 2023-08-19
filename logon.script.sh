167eae7129b98076689558a3b54b952cc394f2fa
Roger Wakeman <rgrwkmn@users.noreply.github.com>
2023-08-18 22:11:40 -0500

    Deal with race condition (#12)

--- logon.script.sh
+++ logon.script.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Update Dock
-# don't set background before updating the dock, it makes the dock update not work sometimes
-/usr/local/bin/dockutil --remove all \
-    --add /Applications/Google\ Chrome.app \
-    --add /Applications/Firefox.app \
-    --add /Applications/Visual\ Studio\ Code.app \
-    --add /Applications/Unity/Unity.app
-
-# ---------------------------------------------
-
 # Set Background
 automator -i /Users/Shared/weallcode-background.png /Users/Shared/setDesktopWallpaper.workflow
 
@@ -17,3 +7,13 @@ automator -i /Users/Shared/weallcode-background.png /Users/Shared/setDesktopWall
 
 # Open Survey
 open https://wac.fyi/survey
+
+# ---------------------------------------------
+
+# Update Dock
+# don't set background and update dock back to back, they seem to cause a race condition as they restart the dock/desktop
+/usr/local/bin/dockutil --remove all \
+    --add /Applications/Google\ Chrome.app \
+    --add /Applications/Firefox.app \
+    --add /Applications/Visual\ Studio\ Code.app \
+    --add /Applications/Unity/Unity.app
